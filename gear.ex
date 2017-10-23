e15124153e8846dfc6dae0b85fd1af8aef1fbcf7
Devtron718 <devin.miranda@gmail.com>
2017-10-23 09:43:27 -0500

    furst

---
+++ gear.ex
@@ -0,0 +1,114 @@
+List of Stuff:
+
+Essential:
+  water_filter
+    tap isnt cool to drink most countries.
+    and buying bottled for all the time sucks.
+    i recommend "https://www.amazon.com/Sawyer-Products-SP103-Filtration-System/dp/B00MPH1LEU/ref=sr_1_1?s=sporting-goods&ie=UTF8&qid=1508768026&sr=1-1&keywords=sawyer+water+filter"
+    since it screws onto bottles
+  small draw_string_style bag
+    dont wanna carry ur laptop-sized bag for adventures in the jungle
+    or casual trips to the grocery store
+  warm_jacket
+    even if mostly in the tropics, plently of places
+    that get cold (especially towards andes)
+  bowl/plate/spoon/fork/mug
+    many airbnbs/hostels wont have shit for u to use
+  shaver
+    if u use electric, wanna briiiing
+  dry_bag
+    when it rains it pours. dont want those
+    computadoras to fry
+  head_lamp
+    supa key, especially if going to more rural areas
+  sleeping mask
+    gunna be many o wierd places u sleep
+    planes + buses + hostels that have a stupid ass street light right outside the window wtf
+  ear plugs (or maybe just headphones)
+    south americas love their freakin fire works. furreal.
+    on fiestas (which seem to be every other week), theyll often
+    fire em off on the half hour throughout the night
+    also, barking dogs. it wont be long before i murder one.
+  kindle
+    i read like 1/2 books a year before south america.
+    now i read 1/2 per month, seriously. you have WAY MORE
+    downtime with trains + buses + planes + nights in small towns
+    or on beaches
+  google project-fi phone plan
+    i LOVE this thing. no need to buy sim cars everycountry you go.
+    project-fi links up with the local network, and is reasonably priced.
+    plus comes with hotspot
+    i usually have better internet than the locals!
+  fast-drying clothes (synthetic)
+    most places dont have drying machines, and hang dry.
+    if ur bouncing around, it sucks to have to wait
+    2-3 days for ur shit to dry when ur bus leaves in 1 hr.
+    synthetic shirts are clutch
+  rain_shell
+    when it rains it pouuurs
+  travel power adapter
+    i have 2. outlets change errywhere, supa key.
+    and the ones the sell in sout america blow.
+  things to charge ur devices
+    extra usb cords/chargers and all taht shit.
+    i lose em all the time and the chargers they sell here
+    usually charge WAY slower so good to have some
+    extras
+  go pro, duh
+    the lil session is clutch since so discreet
+  sunglasses
+  headphones with the mic
+    maybe carry 2 sets o these
+  quick_drying_towel
+    "https://www.amazon.com/s/ref=nb_sb_ss_c_3_12?url=search-alias%3Doutdoor-recreation&field-keywords=quick+dry+towel&sprefix=quick+dry+to%2Celectronics%2C254&crid=3PZ8TJP8Y790A&rh=i%3Aoutdoor-recreation%2Ck%3Aquick+dry+towel"
+  first_aid_kit
+  debit card with no foreign transaction or atm fees
+    i use charles schwabs, it dope
+    also good to only have 1 or 2 gs on it so
+    if stolen not the end of all ur savings
+  credit card with dem points
+
+clothes-wise:
+  7 synthetic shirts
+  3 bball shorts
+  7 socks (3 of which are wool)
+  2 reg shorts
+  1 jeans (might ditch)
+  1 pants
+  1 down jacket (love)
+  1 midlayer (alpaca baby)
+  2 hats (adventure and reg)
+  9 underware (clutch to have a lot)
+  1 rainshell
+  2 collered shirts (fer getting ma swing on)
+  1 dress outfit (mostly just for going back for friends weddings and shit)
+
+Good_to_have:
+  shoe_glue
+    sucks when ur shoes give out. ive had to use it on my
+    nikes + chacos thus far
+  pens
+    would be in category below but this is mainly for
+    filling out the visa shit on the plane. beats having
+    to ask flight_attendants/peeps all the time
+  rope
+    ...maybe more if ur doing backpacking
+  portable rechargable battery
+    got stolen. but was kinda handy while it lasted
+
+clutch_Stuff_you_can_get_in_South_america:
+  garbage bags
+  hat
+  zip locks
+  soccer jersey, when ya need to bleeeend
+  toilet paper.
+    always. carry. toilet paper.
+  coin purse
+    so many coinssss
+
+shit_i_didnt_need_but_stupidly_brought:
+  mosquito net hat. lol
+  hammock
+  mosquito net (maybe debatable)
+  language dictionaries
+  lonley planet
